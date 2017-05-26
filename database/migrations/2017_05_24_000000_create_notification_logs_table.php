907a39d7eb813ff1a39aaa73029cf54ef9892cc3
Yash Barot <yashce04@gmail.com>
2017-05-26 15:19:33 +0530

    Add notification_log table migration

---
+++ database/migrations/2017_05_24_000000_create_notification_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notification_logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->longtext('device_token')->nullable();
+            $table->longtext('notification_message')->nullable();
+            $table->longtext('error_message')->nullable();
+            $table->enum('status', ['success', 'failure']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notification_logs');
+    }
+}
