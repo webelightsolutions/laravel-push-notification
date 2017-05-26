af8fb929f15d59380f2abed8caa96c96b8348be7
Yash Barot <yashce04@gmail.com>
2017-05-26 15:24:49 +0530

    Add Job

---
+++ src/Jobs/PushNotificationJob.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Webelightdev\LaravelPushNotification\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class PushNotificationJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $deviceTokens;
+    protected $message;
+    protected $action;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($deviceTokens, $message, $action)
+    {
+        $this->deviceTokens = $deviceTokens;
+        $this->message = $message;
+        $this->action = $action;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $adapter = config('push-notification.adapter');
+        $objAdapter = new $adapter;
+        $objAdapter->pushNotification($this->deviceTokens, $this->message, $this->action);
+    }
+}
